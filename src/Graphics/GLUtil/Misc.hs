e8e183cc11d1aa8275e2d7402cf9e983afabe30c
Anthony Cowley <acowley@seas.upenn.edu>
2012-01-31 17:22:49 -0500

    Add option to throw an exception carrying any OpenGL errors.
    Tweaked existing error reporting API.

--- src/Graphics/GLUtil/Misc.hs
+++ src/Graphics/GLUtil/Misc.hs
@@ -1,16 +1,39 @@
-module Graphics.GLUtil.Misc where
+{-# LANGUAGE DeriveDataTypeable #-}
+module Graphics.GLUtil.Misc (printError, printErrorMsg, throwError, 
+                             GLError, throwErrorMsg) where
+import Control.Exception (Exception, throwIO)
 import Control.Monad (when)
+import Data.List (intercalate)
+import Data.Typeable (Typeable)
 import Graphics.Rendering.OpenGL
 import System.IO (hPutStrLn, stderr)
 
 -- |Check OpenGL error flags and print them on 'stderr'.
-printErrors :: IO ()
-printErrors = get errors >>= mapM_ (hPutStrLn stderr . ("GL: "++) . show)
+printError :: IO ()
+printError = get errors >>= mapM_ (hPutStrLn stderr . ("GL: "++) . show)
 
 -- |Check OpenGL error flags and print them on 'stderr' with the given
 -- message as a prefix. If there are no errors, nothing is printed.
-printErrorsMsg :: String -> IO ()
-printErrorsMsg msg = do errs <- get errors
-                        when (not (null errs))
-                             (putStrLn msg >> mapM_ printErr errs)
+printErrorMsg :: String -> IO ()
+printErrorMsg msg = do errs <- get errors
+                       when (not (null errs))
+                            (putStrLn msg >> mapM_ printErr errs)
   where printErr = hPutStrLn stderr . ("  GL: "++) . show
+
+data GLError = GLError String deriving (Typeable)
+instance Exception GLError where
+instance Show GLError where
+  show (GLError msg) = "GLError " ++ msg
+
+printGLErrors :: Show a => [a] -> String
+printGLErrors = intercalate "\n  GL: " . ("" :) . map show
+
+throwError :: IO ()
+throwError = do errs <- get errors
+                when (not (null errs))
+                     (throwIO . GLError . tail $ printGLErrors errs)
+
+throwErrorMsg :: String -> IO ()
+throwErrorMsg msg = do errs <- get errors
+                       when (not (null errs))
+                            (throwIO $ GLError (msg++printGLErrors errs))
