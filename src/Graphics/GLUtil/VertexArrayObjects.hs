6acc29ebd37eee4012ab76168b99feecfbf6b5e1
Anthony Cowley <acowley@gmail.com>
2013-02-04 15:38:37 -0500

    Use OpenGL library's VAO definition.

--- src/Graphics/GLUtil/VertexArrayObjects.hs
+++ src/Graphics/GLUtil/VertexArrayObjects.hs
@@ -1,36 +1,22 @@
 -- | A thin layer over OpenGL 3.1+ vertex array objects.
-module Graphics.GLUtil.VertexArrayObjects 
-  (VertexArrayObject(..), VAO, makeVAO, deleteVAO, bindVertexArray) where
-import Foreign.Marshal.Array (allocaArray)
-import Foreign.Marshal.Utils (with)
-import Foreign.Storable (peek)
-import Graphics.Rendering.OpenGL.Raw.Core31
+module Graphics.GLUtil.VertexArrayObjects (makeVAO, withVAO, VAO) where
+import Graphics.Rendering.OpenGL
 
--- |A vertex array object captures OpenGL state needed for drawing a
--- vertex array. It encapsulates the binding of an array buffer and an
--- element buffer, as well as vertex attribute setup.
-newtype VertexArrayObject = VertexArrayObject GLuint
-
--- |Short alias.
+-- |Short alias for 'VertexArrayObject'.
 type VAO = VertexArrayObject
 
 -- |Allocate a 'VertexArrayObject', and initialize it with the
 -- provided action. This action should bind the buffer data, index
 -- data (if necessary), and setup vertex attributes.
 makeVAO :: IO () -> IO VertexArrayObject
-makeVAO m = do vao <- allocaArray 1 $ \ptr -> 
-                        glGenVertexArrays 1 ptr >> peek ptr
-               glBindVertexArray vao
-               m
-               glBindVertexArray 0
-               return $ VertexArrayObject vao
+makeVAO setup = do [vao] <- genObjectNames 1
+                   bindVertexArrayObject $= Just vao
+                   setup
+                   bindVertexArrayObject $= Nothing
+                   return vao
 
--- |Delete a 'VertexArrayObject'. Do not use the VAO after running
--- this action!
-deleteVAO :: VertexArrayObject -> IO ()
-deleteVAO (VertexArrayObject i) = with i $ glDeleteVertexArrays 1
-
--- |Bind a 'VertexArrayObject', or ensure that no VAO is bound.
-bindVertexArray :: Maybe VertexArrayObject -> IO ()
-bindVertexArray (Just (VertexArrayObject i)) = glBindVertexArray i
-bindVertexArray Nothing = glBindVertexArray 0
+-- |Run an action with the given 'VertexArrayObject' bound.
+withVAO :: VertexArrayObject -> IO () -> IO ()
+withVAO vao useIt = do bindVertexArrayObject $= Just vao
+                       useIt
+                       bindVertexArrayObject $= Nothing
