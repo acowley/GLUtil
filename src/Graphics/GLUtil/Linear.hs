5ffaf211eae93f1d726502886d573d50e17c0d89
Anthony Cowley <acowley@gmail.com>
2013-04-24 09:27:05 -0400

    Simplify Linear integration with cpphs.
    We're really interested in a very finite set of instances because GLSL
    only supports a small number of types. Enumerating these instances
    specifically, lets us handle each type distinctly without having
    trouble with representation re-use (e.g. a @V4 GLfloat@ vs a @V4 (V4
    GLfloat)@).
    
    The enumeration is annoyingly repetitive, but some simple string
    splicing makes it manageable. Since we want to concatenate tokens, we
    can't use GHC's simple CPP support, and instead must use cpphs with
    the @--hashes@ option.

--- src/Graphics/GLUtil/Linear.hs
+++ src/Graphics/GLUtil/Linear.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE FlexibleInstances, FlexibleContexts, ScopedTypeVariables #-}
+{-# OPTIONS_GHC -cpp -pgmPcpphs -optP--cpp -optP--hashes #-}
 -- |Support for writing "Linear" types to uniform locations in
 -- shader programs.
-module Graphics.GLUtil.Linear (AsUniform(..), GLComponent(..)) where
+module Graphics.GLUtil.Linear (AsUniform(..)) where
 import Foreign.Marshal.Array (withArray)
 import Foreign.Marshal.Utils (with)
 import Foreign.Ptr (Ptr, castPtr)
@@ -19,30 +20,6 @@ class AsUniform t where
 getUL :: UniformLocation -> GLint
 getUL = unsafeCoerce
 
-class Storable a => GLComponent a where
-  uniform1v    :: UniformLocation -> GLsizei -> Ptr a -> IO ()
-  uniform2v    :: UniformLocation -> GLsizei -> Ptr a -> IO ()
-  uniform3v    :: UniformLocation -> GLsizei -> Ptr a -> IO ()
-  uniform4v    :: UniformLocation -> GLsizei -> Ptr a -> IO ()
-
-instance GLComponent GLint where
-  uniform1v = glUniform1iv . getUL
-  uniform2v = glUniform2iv . getUL
-  uniform3v = glUniform3iv . getUL
-  uniform4v = glUniform4iv . getUL
-  
-instance GLComponent GLuint where
-  uniform1v = glUniform1uiv . getUL
-  uniform2v = glUniform2uiv . getUL
-  uniform3v = glUniform3uiv . getUL
-  uniform4v = glUniform4uiv . getUL
-
-instance GLComponent GLfloat where
-  uniform1v = glUniform1fv . getUL
-  uniform2v = glUniform2fv . getUL
-  uniform3v = glUniform3fv . getUL
-  uniform4v = glUniform4fv . getUL
-
 castVecComponent :: Ptr (t a) -> Ptr a
 castVecComponent = castPtr
 
@@ -50,59 +27,40 @@ castMatComponent :: Ptr (t (f a)) -> Ptr a
 castMatComponent = castPtr
 
 instance AsUniform GLint where
-  x `asUniform` loc = with x $ uniform1v loc 1
+  x `asUniform` loc = with x $ glUniform1iv (getUL loc) 1
 
 instance AsUniform GLuint where
-  x `asUniform` loc = with x $ uniform1v loc 1
+  x `asUniform` loc = with x $ glUniform1uiv (getUL loc) 1
 
 instance AsUniform GLfloat where
-  x `asUniform` loc = with x $ uniform1v loc 1
+  x `asUniform` loc = with x $ glUniform1fv (getUL loc) 1
 
-instance GLComponent a => AsUniform (V2 a) where
-  v `asUniform` loc = with v $ uniform2v loc 1 . castVecComponent
+#define UNIFORMVEC_T(d,ht,glt) instance AsUniform (V ## d ht) where {v `asUniform` loc = with v $ glUniform##d##glt##v (getUL loc) 1 . castVecComponent}
 
-instance GLComponent a => AsUniform (V3 a) where
-  v `asUniform` loc = with v $ uniform3v loc 1 . castVecComponent
+#define UNIFORMVEC(d) UNIFORMVEC_T(d,GLint,i); UNIFORMVEC_T(d,GLuint,ui); UNIFORMVEC_T(d,GLfloat,f)
 
-instance GLComponent a => AsUniform (V4 a) where
-  v `asUniform` loc = with v $ uniform4v loc 1 . castVecComponent
+UNIFORMVEC(2)
+UNIFORMVEC(3)
+UNIFORMVEC(4)
 
 instance AsUniform (M22 GLfloat) where
   m `asUniform` loc = with m
-                    $ glUniformMatrix2fv (getUL loc) 1 0 . castMatComponent
+                    $ glUniformMatrix2fv (getUL loc) 1 1 . castMatComponent
 
 instance AsUniform (M33 GLfloat) where
   m `asUniform` loc = with m
-                    $ glUniformMatrix3fv (getUL loc) 1 0 . castMatComponent
+                    $ glUniformMatrix3fv (getUL loc) 1 1 . castMatComponent
 
 instance AsUniform (M44 GLfloat) where
   m `asUniform` loc = with m
-                    $ glUniformMatrix4fv (getUL loc) 1 0 . castMatComponent
+                    $ glUniformMatrix4fv (getUL loc) 1 1 . castMatComponent
 
--- In order to support uniform arrays, we support lists of vectors and
--- matrices. We do this by tying the dimensionality of each supported
--- finite dimensional vector to a GL call.
+-- Support lists of vectors as uniform arrays of vectors.
 
-class HasComponents t where
-  uniformv :: GLComponent a
-           => t a -> UniformLocation -> GLsizei -> Ptr a -> IO ()
+#define UNIFORMARRAY_T(d,ht,glt) instance AsUniform [V##d ht] where {l `asUniform` loc = withArray l $ glUniform##d##glt##v (getUL loc) (fromIntegral $ length l) . castVecComponent}
 
-instance HasComponents V2 where uniformv _ = uniform2v
+#define UNIFORMARRAY(d) UNIFORMARRAY_T(d,GLint,i); UNIFORMARRAY_T(d,GLuint,ui); UNIFORMARRAY_T(d,GLfloat,f)
 
-instance HasComponents V3 where uniformv _ = uniform3v
-
-instance HasComponents V4 where uniformv _ = uniform4v
-
-instance forall a t. 
-         (AsUniform (t a), GLComponent a, HasComponents t, Storable (t a))
-         => AsUniform [t a] where
-  l `asUniform` loc = withArray l
-                    $ uniformv (undefined::t a) loc (fromIntegral $ length l)
-                    . castVecComponent
-
-instance forall a f t. 
-         (AsUniform (f a), GLComponent a, HasComponents t, Storable (t (f a)))
-         => AsUniform [t (f a)] where
-  l `asUniform` loc = withArray l
-                    $ uniformv (undefined::t a) loc (fromIntegral $ length l)
-                    . castMatComponent
+UNIFORMARRAY(2)
+UNIFORMARRAY(3)
+UNIFORMARRAY(4)
