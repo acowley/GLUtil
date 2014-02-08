bb74b578e7ce941e94abb0bcc49eaa6a5910f1d2
Anthony Cowley <acowley@gmail.com>
2014-02-07 19:58:54 -0500

    Generalized withViewport to MonadIO.

--- src/Graphics/GLUtil/Viewport.hs
+++ src/Graphics/GLUtil/Viewport.hs
@@ -1,14 +1,16 @@
 -- | Helpers for working with OpenGL viewports.
 module Graphics.GLUtil.Viewport where
+import Control.Monad.IO.Class
 import Graphics.Rendering.OpenGL
 
 -- | @withViewport pos sz m@ runs the action @m@ after setting the
 -- viewport with the given 'Position' and 'Size'. The viewport is
 -- reset to its original state after the action is run, and the result
 -- of the action is returned.
-withViewport :: Position -> Size -> IO a -> IO a
-withViewport p s m = do oldVP <- get viewport
-                        viewport $= (p,s)
+withViewport :: MonadIO m => Position -> Size -> m a -> m a
+withViewport p s m = do oldVP <- liftIO $ do oldVP <- get viewport
+                                             viewport $= (p,s)
+                                             return oldVP
                         r <- m
-                        viewport $= oldVP
+                        liftIO $ viewport $= oldVP
                         return r
